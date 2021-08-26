c5c87c5cfa985cc3eb16f0664c533c22e6986218
robin <robinstark360@gmail.com>
2021-08-26 19:20:11 +0545

    Fixed Cursor isuues

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from pygame.locals import *
 #Initialize
 pygame.init()
 WIN_SIZE = (640,480)
-white = (255, 255, 255)
-black = (0, 0, 0)
 main_clock = pygame.time.Clock()
 window = pygame.display.set_mode(WIN_SIZE)
 pygame.display.set_caption('Tic Tac Toe')
+cursor = pygame.Rect(100, 100, 100, 100)
+
+#colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+gray = (77, 77, 77)
 
 #Font
 font = pygame.font.Font('Roboto-Light.ttf', 50)
@@ -31,31 +35,33 @@ def draw_board():
     window.blit(board,(0,0))
 
 def draw_cursor():
-    cursor_color = (155, 25, 20)
-    cursor_width = 50
-    pygame.draw.rect(window, cursor_color, (x, y, 5, 5), cursor_width)
-  
+    cursor_color = (200, 100, 150)
+    pygame.draw.rect(window, cursor_color, cursor, 100, 4)
+
+def move_cursor():
+    pass
+
 def particle_effect(x, y):
-     particles.append([[x, y], [random.randint(-20,20)/10*-1, random.randint(-20,20)/10*-1],random.randint(4,6)])
+     particles.append([[x, y], [random.randint(-20,20)/10*-1, random.randint(-20,20)/10*-1],random.randint(50, 70)])
 
      for particle in particles:
          particle[0][0] += particle[1][0]
          particle[0][1] += particle[1][1]
          particle[2] -= 0.2
-         pygame.draw.circle(window, white, [int(particle[0][0]), int(particle[0][1])],int(particle[2]))
+         pygame.draw.circle(window, gray, [int(particle[0][0]), int(particle[0][1])],int(particle[2]))
      if particle[2] <= 0:
          particles.remove(particle)
 
 
 #Main game loop
 while True:
+    window.fill(black)
+    particle_effect(0, 0)
+    particle_effect(WIN_SIZE[0], WIN_SIZE[1])
+    window.blit(top_render,(185,15)) 
+    draw_board()
+    draw_cursor()
     for event in pygame.event.get():
-        x, y = pygame.mouse.get_pos() #Mouse position
-        window.fill(black)
-        window.blit(top_render,(185,15)) 
-        draw_board()
-        draw_cursor()
-        particle_effect(x, y)
         if event.type == QUIT:
             sys.exit()
 
@@ -63,11 +69,8 @@ while True:
             if event.key == K_ESCAPE:
                 sys.exit()
         
-        if event.type == pygame.MOUSEBUTTONUP :
-                board_images.append([x, y])
-                print(board_images)
-        pygame.display.update()
-        main_clock.tick(60)
+    pygame.display.update()
+    main_clock.tick(60)
     
 
 
